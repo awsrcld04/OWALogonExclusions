19e83613adaadcc5f0dffdf68dec9b9702de73af
awsrcld04 <akil.woolfolk@gmail.com>
2025-04-07 14:25:54 -0400

    Removed funcLicenseActivation function block

--- OWALogonExclusions.cs
+++ OWALogonExclusions.cs
@@ -191,53 +191,6 @@ namespace OWALogonExclusions
             } // end of catch block on new StreamReader("sotfwlic.dat")
         }
 
-        static bool funcLicenseActivation()
-        {
-            try
-            {
-                if (funcCheckForFile("TurboActivate.dll"))
-                {
-                    if (funcCheckForFile("TurboActivate.dat"))
-                    {
-                        TurboActivate.VersionGUID = "4935355894e0da3d4465e86.37472852";
-
-                        if (TurboActivate.IsActivated())
-                        {
-                            return true;
-                        }
-                        else
-                        {
-                            if (funcPortableLicenseCheck())
-                            {
-                                return true;
-                            }
-                            else
-                            {
-                                Console.WriteLine("A license for this product has not been activated.");
-                                return false;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("TurboActivate.dat is required and could not be found.");
-                        return false;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("TurboActivate.dll is required and could not be found.");
-                    return false;
-                }
-            }
-            catch (Exception ex)
-            {
-                MethodBase mb1 = MethodBase.GetCurrentMethod();
-                funcGetFuncCatchCode(mb1.Name, ex);
-                return false;
-            }
-        }
-
         static bool funcPortableLicenseCheck()
         {
             try
